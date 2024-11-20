442a61251dd4dde591322ac7a8a88d19110a261f
PopykTima <timapopyk@gmail.com>
2024-11-20 10:38:50 +0200

    Add files via upload

---
+++ EchoBot.py
@@ -0,0 +1,21 @@
+import telebot
+
+API_TOKEN = '7674474171:AAF71ghIszAa0cH_ZwBVz0PLsS-nt0EV81s'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=[ 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()  
